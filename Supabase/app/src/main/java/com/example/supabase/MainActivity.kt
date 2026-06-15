4723721206c78387e3c54fb02634fc1d08a6a491
codecraft732 <alishbaimran980@gmail.com>
2026-06-15 10:22:16 +0500

    new project added

---
+++ Supabase/app/src/main/java/com/example/supabase/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.supabase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.supabase.ui.theme.SupabaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SupabaseTheme {
+
+            }
+        }
+    }
+}
